f51b6b5b57c0f2c49a9d2e334f14e172ef34cb04
KuoJiuSheng <love1357983@gmail.com>
2017-08-16 18:04:38 +0800

    updata testing_project.sh add judgment directory exists func

--- testing_project.sh
+++ testing_project.sh
@@ -10,10 +10,37 @@ test_device_serial_number=$3
 for aapt_path in ${ANDROID_HOME}/build-tools/*/; do break; done
 export PATH="$PATH:${aapt_path}"
 
+# check `testing_result` directory exists.
+if [ ! -d "testing_result" ]; then
+
+    mkdir testing_result
+
+fi
+
+# check `testing_result/$test_project_name` directory exists.
+if [ ! -d "testing_result/$test_project_name" ]; then
+
+    mkdir testing_result/$test_project_name
+
+fi
+
+# check `testing_result/$test_project_name/$test_data` directory exists
+if [ ! -d "testing_result/$test_project_name/$test_data" ]; then
+
+    mkdir testing_result/$test_project_name/$test_data
+
+fi
+
+# check `testing_result/$test_project_name/$test_data/$test_device_serial_number` directory exists
+if [ ! -d "testing_result/$test_project_name/$test_data/$test_device_serial_number" ]; then
+
+    mkdir testing_result/$test_project_name/$test_data/$test_device_serial_number
+
+fi
+
 if [ -d "uploads/$test_project_name" ]; then
 
     #echo "Directory $test_project_name exists."
-
     app_file=`ls uploads/$test_project_name/apk_file`
 
     apk_package=`aapt dump badging uploads/$test_project_name/apk_file/$app_file | grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
@@ -22,7 +49,7 @@ if [ -d "uploads/$test_project_name" ]; then
 
     apk_test_package=`aapt dump badging uploads/$test_project_name/apk_test_file/$apk_test_file | grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
 
-    # 因為在此無法立即回應，所以我們先將我們的結果放置在設定好的文件當中
+    # Because we can not respond immediately, so we first put our results in the set of documents.
     echo `adb -s $test_device_serial_number push uploads/$test_project_name/apk_file/$app_file data/local/tmp/$apk_package` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/apk_push
 
     echo `adb -s $test_device_serial_number shell pm install -r "/data/local/tmp/$apk_package"` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/apk_install
